4f77b89e262eadcda7ed21ef359e5213fceebd6a
yaokai <yaokai729@outlook.com>
2019-08-20 22:04:28 +0800

    users完成

--- src/main.js
+++ src/main.js
@@ -6,10 +6,23 @@ import ElementUI from 'element-ui';
 import 'element-ui/lib/theme-chalk/index.css';
 import axios from 'axios';
 
+Vue.config.productionTip = false
+// 使用ElementUI
 Vue.use(ElementUI)
 
-axios.defaults.baseURL = ""
-Vue.config.productionTip = false
+// 配置全局axios请求的baseURL
+// axios.defaults.baseURL = "http://3.112.132.86:8888/api/private/v1/"
+axios.defaults.baseURL = 'http://3.112.132.86:8878'
+
+// 这个方法（axios拦截器）什么时候出发？发送axios请求时触发
+// axios.interceptors.request.use(config => {
+//   let token = localStorage.getItem('token')
+//   if (token) {
+//     config.header['Authorization'] = token
+//   }
+//   return config
+// })
+
 Vue.prototype.axios = axios
 
 new Vue({
