bdbc182500ff902ca34f25381f6c93b21052c2cc
yaokai <yaokai729@outlook.com>
2019-09-02 22:35:34 +0800

    rights role 点击按钮出现分配权限

--- src/views/rights/role.vue
+++ src/views/rights/role.vue
@@ -2,13 +2,14 @@
   <div>
     <bread-crumb level1="权限管理" level2="角色列表"></bread-crumb>
     <el-button class="addRoleBtn" type="success" plain>添加角色</el-button>
+    <!-- 角色列表 -->
     <el-table :data="roleList" border style="width: 100%">
       <el-table-column type="expand" width="150">
         <template v-slot="scope">
           <el-row v-for="(level1, i) in scope.row.children" :key="i">
             <el-col :span="4">
               <el-tag
-                @close="deleRight(scope.row.id, level1.id)"
+                @close="deleRight(scope.row, level1.id)"
                 closable
                 type="success"
               >{{level1.authName}}</el-tag>
@@ -17,16 +18,12 @@
             <el-col :span="20">
               <el-row v-for="(level2, i) in level1.children" :key="i">
                 <el-col :span="4">
-                  <el-tag
-                    @close="deleRight(scope.row.id, level1.id)"
-                    closable
-                    type
-                  >{{level2.authName}}</el-tag>
+                  <el-tag @close="deleRight(scope.row, level3.id)" closable type>{{level2.authName}}</el-tag>
                   <i class="el-icon-arrow-right"></i>
                 </el-col>
                 <el-col :span="20">
                   <el-tag
-                    @close="deleRight(scope.row.id, level1.id)"
+                    @close="deleRight(scope.row, level3.id)"
                     closable
                     type="warning"
                     v-for="(level3, i) in level2.children"
@@ -44,22 +41,37 @@
       <el-table-column prop="roleDesc" label="角色描述" width="250"></el-table-column>
       <el-table-column prop="address" label="操作">
         <template v-slot="scope">
-          <el-button type="primary" icon="el-icon-edit" circle @click="showEditUserDia(scope.row)"></el-button>
-          <el-button
-            type="danger"
-            icon="el-icon-delete"
-            circle
-            @click="showDelUserMsgBox(scope.row.id)"
-          ></el-button>
-          <el-button
-            type="success"
-            icon="el-icon-check"
-            circle
-            @click="showSetUserRoleDia(scope.row)"
-          ></el-button>
+          <el-button type="primary" icon="el-icon-edit" circle @click="showRightDia(scope.row)"></el-button>
+          <el-button type="danger" icon="el-icon-delete" circle @click="remove(scope.row)"></el-button>
+          <el-button type="success" icon="el-icon-check" circle @click="showSetRightDia(scope.row)"></el-button>
         </template>
       </el-table-column>
     </el-table>
+    <!-- 修改权限 -->
+    <el-dialog title="修改权限" :visible.sync="dialogFormVisibleRight">
+      <!-- 树形结构
+      show-checkbox -> 选择框
+      node-key 每个节点的唯一标识
+      default-expand-keys 默认展开 [要展开的节点的id]
+      default-checked-keys 默认展开 [要选择的节点的id]
+      props 配置项 {label, children}
+      label节点的文字标题和children节点的子节点
+      值都类源于data绑定的数据源中的该数据的key名 'label'和 'chldren'-->
+      <!-- 
+      -->
+      <el-tree
+        :data="treeList"
+        show-checkbox
+        node-key="id"
+        :props="defaultProps"
+        default-expand-all
+        :default-checked-keys="arrCheck"
+      ></el-tree>
+      <div slot="footer" class="dialog-footer">
+        <el-button @click="dialogFormVisibleRight = false">取 消</el-button>
+        <el-button type="primary" @click="setRoleRight()">确 定</el-button>
+      </div>
+    </el-dialog>
   </div>
 </template>
 
@@ -67,21 +79,51 @@
 export default {
   data() {
     return {
-      roleList: []
+      roleList: [],
+      dialogFormVisibleRight: false,
+      // 树形结构数据
+      treeList: [],
+      defaultProps: {
+        label: "authName",
+        childrem: "children"
+      },
+      arrExpand: [],
+      arrCheck: [],
+      currRoleId: -1
     };
   },
   methods: {
     getRoleList() {
       this.axios.get(`roles`).then(res => {
-        console.log(res);
         this.roleList = res.data.data;
       });
     },
-    deleRight(roleId, rightId) {
-      this.axios.delete(`roles/${roleId}/rights/${rightId}`).then(res => {
-        console.log(res);
-        this.getRoleList();
+    deleRight(role, rightId) {
+      this.axios.delete(`roles/${role.id}/rights/${rightId}`).then(res => {
+        role.children = res.data.data;
       });
+    },
+    async showSetRightDia(role) {
+      this.currRoleId = role.id;
+      const res = await this.axios.get(`rights/tree`);
+      console.log(res);
+      this.treeList = res.data.data;
+      // 选中角色拥有的权限
+      let arrtemp2 = [];
+      role.children.forEach(item1 => {
+        // arrtemp2.push(item1.id);
+        item1.children.forEach(item2 => {
+          // arrtemp2.push(item2.id);
+          item2.children.forEach(item3 => {
+            arrtemp2.push(item3.id);
+          });
+        });
+      });
+      this.arrCheck = arrtemp2;
+      this.dialogFormVisibleRight = true;
+    },
+    setRoleRight() {
+      // this.axios.post(`roles/${this.currRoleId}/rights`, rid).then(res => {});
     }
   },
   created() {
